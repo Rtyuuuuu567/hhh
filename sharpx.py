a24089888ff7af535976090ec5b7e81756433891
Rtyuuuuu567 <rg7799482@gmail.com>
2024-10-30 18:02:54 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7668398361:AAH4VfxcpE5tsdlLyZfZ1cD2PZb1NQsi2gA")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Miss_queen19")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5859040781")
+
+
+
+
+
+  
